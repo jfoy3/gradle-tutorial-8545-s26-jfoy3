bac6bd8ee77e516c24a99cb11401c23f3723805d
Julia Foy <119273175+jfoy3@users.noreply.github.com>
2026-03-25 12:30:50 -0400

    add plug-in

--- tutorial/app/build.gradle.kts
+++ tutorial/app/build.gradle.kts
@@ -8,6 +8,8 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    // Apply the maven publish plugin
+    id("maven-publish")
 }
 
 repositories {
